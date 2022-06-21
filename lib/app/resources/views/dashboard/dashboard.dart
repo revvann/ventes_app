604959180709c55d292b37ba6f3ef4e54fa634b7
The Mighty Udin <email>
2022-06-21 10:29:51 +0700

    selesai

--- lib/app/resources/views/dashboard/dashboard.dart
+++ lib/app/resources/views/dashboard/dashboard.dart
@@ -35,183 +35,194 @@ class DashboardView extends View<DashboardStateController> {
       body: SafeArea(
         child: RefreshIndicator(
           onRefresh: () async => state.refreshStates(),
-          child: CustomScrollView(slivers: [
-            SliverFillRemaining(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 120,
-                    width: double.infinity,
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        _AppBar(),
-                        _TopPanel(),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 125,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: RegularSize.m,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Row(
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 120,
+                        width: double.infinity,
+                        child: Stack(
+                          clipBehavior: Clip.none,
                           children: [
-                            _MenuItem(
-                              color: RegularColor.purple,
-                              icon: "assets/svg/marker.svg",
-                              text: "Nearby",
-                              onTap: () => state.bottomNavigation.currentIndex = Views.nearby,
-                            ),
-                            SizedBox(
-                              width: RegularSize.s,
-                            ),
-                            _MenuItem(
-                              color: RegularColor.yellow,
-                              icon: "assets/svg/calendar.svg",
-                              text: "Schedule",
-                              onTap: () => state.bottomNavigation.currentIndex = Views.schedule,
-                            ),
-                            SizedBox(
-                              width: RegularSize.s,
-                            ),
-                            _MenuItem(
-                              color: RegularColor.cyan,
-                              icon: "assets/svg/attendance.svg",
-                              text: "Attendance",
-                              onTap: () async {
-                                Get.find<TaskHelper>().confirmPush(state.property.task.copyWith<bool>(
-                                  message: "Are you sure you want to go to attendance?",
-                                  onFinished: (result) {
-                                    print(result);
+                            _AppBar(),
+                            _TopPanel(),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 125,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: RegularSize.m,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Row(
+                              children: [
+                                _MenuItem(
+                                  color: RegularColor.purple,
+                                  icon: "assets/svg/marker.svg",
+                                  text: "Nearby",
+                                  onTap: () => state.bottomNavigation.currentIndex = Views.nearby,
+                                ),
+                                SizedBox(
+                                  width: RegularSize.s,
+                                ),
+                                _MenuItem(
+                                  color: RegularColor.yellow,
+                                  icon: "assets/svg/calendar.svg",
+                                  text: "Schedule",
+                                  onTap: () => state.bottomNavigation.currentIndex = Views.schedule,
+                                ),
+                                SizedBox(
+                                  width: RegularSize.s,
+                                ),
+                                _MenuItem(
+                                  color: RegularColor.cyan,
+                                  icon: "assets/svg/attendance.svg",
+                                  text: "Attendance",
+                                  onTap: () async {
+                                    Get.find<TaskHelper>().confirmPush(state.property.task.copyWith<bool>(
+                                      message: "Are you sure you want to go to attendance?",
+                                      onFinished: (result) {
+                                        print(result);
+                                      },
+                                    ));
                                   },
-                                ));
-                              },
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: RegularSize.s,
+                            ),
+                            Row(
+                              children: [
+                                _MenuItem(
+                                  color: RegularColor.pink,
+                                  icon: "assets/svg/daily-visit.svg",
+                                  text: "Daily Visit",
+                                  onTap: () {},
+                                ),
+                                SizedBox(
+                                  width: RegularSize.s,
+                                ),
+                                _MenuItem(
+                                  color: RegularColor.red,
+                                  icon: "assets/svg/prospect.svg",
+                                  text: "Prospect",
+                                  onTap: () => state.bottomNavigation.currentIndex = Views.prospect,
+                                ),
+                                SizedBox(
+                                  width: RegularSize.s,
+                                ),
+                                _MenuItem(
+                                  color: RegularColor.gray,
+                                  icon: "assets/svg/user.svg",
+                                  text: "Profile",
+                                  onTap: () => state.bottomNavigation.currentIndex = Views.profile,
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        SizedBox(
-                          height: RegularSize.s,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: RegularSize.m,
                         ),
-                        Row(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
                           children: [
-                            _MenuItem(
-                              color: RegularColor.pink,
-                              icon: "assets/svg/daily-visit.svg",
-                              text: "Daily Visit",
-                              onTap: () {},
+                            SizedBox(
+                              height: RegularSize.l,
+                            ),
+                            Text(
+                              "Plan For You",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                color: RegularColor.primary,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                             SizedBox(
-                              width: RegularSize.s,
+                              height: RegularSize.m,
                             ),
-                            _MenuItem(
-                              color: RegularColor.red,
-                              icon: "assets/svg/prospect.svg",
-                              text: "Prospect",
-                              onTap: () => state.bottomNavigation.currentIndex = Views.prospect,
-                            ),
-                            SizedBox(
-                              width: RegularSize.s,
-                            ),
-                            _MenuItem(
-                              color: RegularColor.gray,
-                              icon: "assets/svg/user.svg",
-                              text: "Profile",
-                              onTap: () => state.bottomNavigation.currentIndex = Views.profile,
-                            ),
+                            _buildTitleHeader("Nearby Customers"),
                           ],
                         ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: RegularSize.m,
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        SizedBox(
-                          height: RegularSize.l,
-                        ),
-                        Text(
-                          "Plan For You",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            color: RegularColor.primary,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        SizedBox(
-                          height: RegularSize.m,
-                        ),
-                        _buildTitleHeader("Nearby Customers"),
-                      ],
-                    ),
-                  ),
-                  if (state.dataSource.customers.isNotEmpty) ...[
-                    SizedBox(
-                      height: 148,
-                      child: Obx(() {
-                        return ListView.builder(
-                          physics: BouncingScrollPhysics(),
-                          itemCount: state.dataSource.customers.length,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (_, index) {
-                            BpCustomer customer = state.dataSource.customers[index];
-                            double mRight = 0;
-                            if (index == 9) {
-                              mRight = 16;
-                            }
-                            return CustomerCard(
-                              image: NetworkImage(customer.sbccstmpic ?? ""),
-                              margin: EdgeInsets.only(
-                                left: 16,
-                                right: mRight,
-                                top: 24,
-                                bottom: 24,
+                      ),
+                      Obx(() {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            if (state.dataSource.customers.isNotEmpty) ...[
+                              SizedBox(
+                                height: 148,
+                                child: Obx(() {
+                                  return ListView.builder(
+                                    physics: BouncingScrollPhysics(),
+                                    itemCount: state.dataSource.customers.length,
+                                    scrollDirection: Axis.horizontal,
+                                    itemBuilder: (_, index) {
+                                      BpCustomer customer = state.dataSource.customers[index];
+                                      double mRight = 0;
+                                      if (index == 9) {
+                                        mRight = 16;
+                                      }
+                                      return CustomerCard(
+                                        image: NetworkImage(customer.sbccstmpic ?? ""),
+                                        margin: EdgeInsets.only(
+                                          left: 16,
+                                          right: mRight,
+                                          top: 24,
+                                          bottom: 24,
+                                        ),
+                                        width: 250,
+                                        title: customer.sbccstmname,
+                                        type: customer.sbccstm?.cstmtype?.typename ?? "",
+                                        radius: (customer.radius! / 1000).toStringAsFixed(2) + " KM",
+                                      );
+                                    },
+                                  );
+                                }),
                               ),
-                              width: 250,
-                              title: customer.sbccstmname,
-                              type: customer.sbccstm?.cstmtype?.typename ?? "",
-                              radius: (customer.radius! / 1000).toStringAsFixed(2) + " KM",
-                            );
-                          },
+                            ] else ...[
+                              SizedBox(
+                                height: RegularSize.s,
+                              ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: RegularSize.m,
+                                ),
+                                child: Text(
+                                  "There is no customer in this area",
+                                  style: TextStyle(
+                                    color: RegularColor.dark,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ],
                         );
                       }),
-                    ),
-                  ] else ...[
-                    SizedBox(
-                      height: RegularSize.s,
-                    ),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      padding: EdgeInsets.symmetric(
-                        horizontal: RegularSize.m,
+                      SizedBox(
+                        height: RegularSize.xl,
                       ),
-                      child: Text(
-                        "There is no customer in this area",
-                        style: TextStyle(
-                          color: RegularColor.dark,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ],
-                  SizedBox(
-                    height: RegularSize.xl,
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ]),
+            ],
+          ),
         ),
       ),
     );
